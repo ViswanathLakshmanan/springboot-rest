71b719a85ce3f3ca06139ca94c0f85fb35844c31
viswanath <viswa.lsk@gmail.com>
2020-05-13 20:34:06 +0530

    first commit

---
+++ src/main/java/com/sampletask/api/bkuprstore/BkuprstoreApplication.java
@@ -0,0 +1,13 @@
+package com.sampletask.api.bkuprstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkuprstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkuprstoreApplication.class, args);
+	}
+
+}
